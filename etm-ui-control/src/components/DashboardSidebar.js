e6a25c3be8246de4a715ed119475c4a82f69bd76
jiny7788 <jiny7788@naver.com>
2022-05-23 22:13:41 +0900

    필요없는 소스 정리

--- etm-ui-control/src/components/DashboardSidebar.js
+++ etm-ui-control/src/components/DashboardSidebar.js
@@ -16,42 +16,42 @@ import { NavItem } from './nav-item';
 const items = [
   {
     href: '/',
-    icon: (<ChartBarIcon fontSize="small" />),
+    icon: <ChartBarIcon fontSize="small" />,
     title: 'Dashboard'
   },
   {
     href: '/customers',
-    icon: (<UsersIcon fontSize="small" />),
+    icon: <UsersIcon fontSize="small" />,
     title: 'Customers'
   },
   {
     href: '/products',
-    icon: (<ShoppingBagIcon fontSize="small" />),
+    icon: <ShoppingBagIcon fontSize="small" />,
     title: 'Products'
   },
   {
     href: '/account',
-    icon: (<UserIcon fontSize="small" />),
+    icon: <UserIcon fontSize="small" />,
     title: 'Account'
   },
   {
     href: '/settings',
-    icon: (<CogIcon fontSize="small" />),
+    icon: <CogIcon fontSize="small" />,
     title: 'Settings'
   },
   {
     href: '/login',
-    icon: (<LockIcon fontSize="small" />),
+    icon: <LockIcon fontSize="small" />,
     title: 'Login'
   },
   {
     href: '/register',
-    icon: (<UserAddIcon fontSize="small" />),
+    icon: <UserAddIcon fontSize="small" />,
     title: 'Register'
   },
   {
     href: '/404',
-    icon: (<XCircleIcon fontSize="small" />),
+    icon: <XCircleIcon fontSize="small" />,
     title: 'Error'
   }
 ];
